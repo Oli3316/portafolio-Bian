26e778e116a39957b72cb6aafcbd96a3240e4a8d
Oliver Baker <oliverbaker3316@gmail.com>
2022-04-21 11:43:23 -0300

    Realice cambios en el Sub menu DropDown

--- JavaScript.js
+++ JavaScript.js
@@ -6,8 +6,7 @@ window.addEventListener("scroll", function () {
 });
 function saludar() {
   Swal.fire({
-    title: "Hola, soy Bianca! Gracias por visitar mi página 😄",
-    text: "Visita mis redes sociales!",
+    title: "Hola, soy Bianca! Gracias por visitar mi espacio 😄",
     imageUrl:
       "https://media-exp1.licdn.com/dms/image/D4E35AQEinjpknmpR-A/profile-framedphoto-shrink_800_800/0/1628767583766?e=2147483647&v=beta&t=KiBHjO7peOLIRBxTARoi9iFVmEyM24qjiwsOp1RJsQU",
     imageWidth: 200,
