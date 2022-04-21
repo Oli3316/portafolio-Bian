2b8d96e53ec26b96d08bf64c0ee61e9679fdbf77
Oliver Baker <oliverbaker3316@gmail.com>
2022-04-21 00:19:54 -0300

    realice cambios en el alert sweat

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,19 @@
+saludar();
+
 window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("abajo", window.scrollY > 20);
 });
-
-const saludo = document.getElementsByTagName("body");
-saludo.addEventListener("scroll", () => {
+function saludar() {
   Swal.fire({
-    title: "Error!",
-    text: "Do you want to continue",
-    icon: "error",
-    confirmButtonText: "Cool",
+    title: "Hola, soy Bianca! Gracias por visitar mi página 😄",
+    text: "Visita mis redes sociales!",
+    imageUrl:
+      "https://media-exp1.licdn.com/dms/image/D4E35AQEinjpknmpR-A/profile-framedphoto-shrink_800_800/0/1628767583766?e=2147483647&v=beta&t=KiBHjO7peOLIRBxTARoi9iFVmEyM24qjiwsOp1RJsQU",
+    imageWidth: 200,
+    imageHeight: 200,
+    confirmButtonText: "Ok",
+    background: "black",
+    color: "#fff",
   });
-});
+}
