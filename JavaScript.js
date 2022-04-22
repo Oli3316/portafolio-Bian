143a3774e89a78a1bdd8aa36753c4d44042d4cad
Oliver Baker <oliverbaker3316@gmail.com>
2022-04-22 19:55:25 -0300

    realice cambios en el boton toggle del navbar y en el sweat alert de bienvenida

--- JavaScript.js
+++ JavaScript.js
@@ -6,14 +6,14 @@ window.addEventListener("scroll", function () {
 });
 function saludar() {
   Swal.fire({
-    title: "Hola, soy Bianca! Gracias por visitar mi espacio 😄",
+    title: "Gracias por visitar mi espacio 😄",
     imageUrl:
       "https://media-exp1.licdn.com/dms/image/D4E35AQEinjpknmpR-A/profile-framedphoto-shrink_800_800/0/1628767583766?e=2147483647&v=beta&t=KiBHjO7peOLIRBxTARoi9iFVmEyM24qjiwsOp1RJsQU",
-    imageWidth: 200,
-    imageHeight: 200,
-    confirmButtonText: "Ok",
-    background: "black",
-    color: "#fff",
+    imageWidth: 300,
+    imageHeight: 300,
+    showConfirmButton: false,
+    timer: 2000,
+    customClass: "sweetAlert sweetAlert2",
   });
 }
 
